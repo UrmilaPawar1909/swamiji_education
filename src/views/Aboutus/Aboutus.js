be00de6afe55874cd7f5e59c410da63e2b5e483f
gita321 <gitanjalikokate921@gmail.com>
2024-01-16 16:15:23 -1200

    adding about us

--- src/views/Aboutus/Aboutus.js
+++ src/views/Aboutus/Aboutus.js
@@ -8,8 +8,38 @@ function Aboutus() {
   return (
     <>
     <Navbar/>
+    <div className='about-container'>
     <h1 className='skheading'>SK Education & Investment Hub...🎯</h1>
     <img src={Img} className='skimage' />
+    <div className='main-container'>
+     <div className='container-child'>
+      <img src={Img} className='child-container-image'/>
+     </div>
+     <div className='container-child'>
+      <h1 className='heading-2'> Welcome to SK Education & Investment Hub...🤝!!</h1>
+      <p className='heading-info'>As the CEO at ‘Power of Stocks’ Subasish oversees the business, and overall development. He also handles strategic planning, marketing, finance management, and digital growth. 
+Prior to his entrepreneurial debut, he was an equity research analyst and an active trader covering all aspects of market, in his previous role. </p>
+     </div>
+    </div>
+    <div className='main-container'>
+     <div className='container-child'>
+      <h1 className='heading-2'>Our Expert...!</h1>
+      <p className='heading-info'>As the CEO at ‘Power of Stocks’ Subasish oversees the business, and overall development. He also handles strategic planning, marketing, finance management, and digital growth. 
+Prior to his entrepreneurial debut, he was an equity research analyst and an active trader covering all aspects of market, in his previous role. </p>
+<button className='button'> Read More </button>
+     </div>
+     <div className='container-child'>
+      <img src={Img} className='child-container-image'/>
+     </div>
+    </div>
+    <div className='main-container'>
+     <div className='container-child'>
+      <img src={Img} className='child-container-image'/>
+     </div>
+     <div className='container-child'></div>
+    </div>
+    
+    </div>
      <Footer/>
     </>
    
