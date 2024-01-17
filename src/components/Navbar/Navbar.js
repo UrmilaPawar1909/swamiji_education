b970e3ac5a5cfc955f9b89a77a036357660d99b4
UrmilaPawar1909 <urmilapawar221@gmail.com>
2024-01-17 13:47:27 +0530

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,29 +14,35 @@ export default function Navbar() {
     <div className="navbar">
             
             <img src={img1} className="brand-logo"/>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
             <li className="nav-item fw-medium ">
-            <Link className="nav-link fs-4 ms-5" to="/">Home</Link>
+            <Link className="nav-link fs-4 ms-3" to="/">Home</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/about">About Us</Link>
+            <Link className="nav-link fs-4 ms-3" to="/about">About Us</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/training">Training</Link>
+            <Link className="nav-link fs-4 ms-3" to="/training">Training</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/opendemataccount">Open a Demat Account</Link>
+            <Link className="nav-link fs-4 ms-3" to="/opendemataccount">Open a Demat Account</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/review">Review</Link>
+            <Link className="nav-link fs-4 ms-3" to="/review">Review</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/gallery">Gallery</Link>
+            <Link className="nav-link fs-4 ms-3" to="/gallery">Gallery</Link>
             </li>
             <li className="nav-item fw-medium">
-            <Link className="nav-link fs-4 ms-5" to="/contact">Contact Us</Link>
+            <Link className="nav-link fs-4 ms-3" to="/contact">Contact Us</Link>
             </li>
             </ul>
+            </div>
            
 
         </div>
