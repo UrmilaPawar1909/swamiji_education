18ced337d0b62109b226f10ec70af26873ed7b60
gita321 <gitanjalikokate921@gmail.com>
2024-01-17 15:25:16 -1200

    Footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,9 +13,23 @@ function Footer()
   return(
 
     <>
-    <div className="footer">
-      <p>All right reserved &copy; 2023</p>
-    </div>
+    <footer className="mainfooter footer" role="contentinfo">
+      <div className="footer-middle">
+        <div className="container">
+          <p className="text-center">&copy; 2024, SK Education & Investment Hub. All Rghts Reserved. </p>
+          <div className="row">
+            <div className="col-md-12 copy">
+              <p className="text-center">
+                <a className="ms-1">Disclamer<span className="ms-1">|</span></a>
+                <a className="ms-1">Terms and condition<span className="ms-1">|</span></a>
+                <a className="ms-1">Cancellation<span className="ms-1">|</span></a>
+                <a className="ms-1">Refund Policy<span className="ms-1">|</span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
     
     </>
   )
