095688adef510e5c8b5ce6755014979a89897005
gita321 <gitanjalikokate921@gmail.com>
2024-01-17 12:37:16 -1200

    adding some info about demet account

--- src/views/Openademataccount/Openademataccount.js
+++ src/views/Openademataccount/Openademataccount.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Openademataccount.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import Image from './Demetimage/Dementaccount.jpeg';
 
 function Openademataccount() {
   return (
     <>
     <Navbar/>
-    <div>Openademataccount</div>
+    <div className='dement-container'>
+      <img src={Image} className='demet-image'/>
+    </div>
     <Footer/>
     </>
   )
