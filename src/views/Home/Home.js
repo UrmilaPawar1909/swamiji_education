d65caee0de5ea5fcf9a216b2efa196d9898fe7f6
UrmilaPawar1909 <urmilapawar221@gmail.com>
2024-01-25 10:07:27 +0530

    Home page Updated

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,32 +5,39 @@ import Footer from '../../components/Footer/Footer';
 import StockImg1 from './Img/stockmarketimg1.jpg';
 import StockImg2 from './Img/stockmarketimg2.webp';
 import StockImg3 from './Img/stockmarketimg3.jpeg';
-import DemetImg from '../Openademataccount/Demetimage/homedemetmain.jpg'
+import DemetImg from '../Openademataccount/Demetimage/homedemetmain.jpg';
 
 function Home() {
   return (
     <>
       <Navbar />
-      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="2000">
-            <img src={StockImg1} class="d-block slider-img  w-100" />
-          </div>
-          <div class="carousel-item " data-bs-interval="2000">
-            <img src={StockImg2} class="d-block slider-img w-100" />
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src={StockImg3} class="d-block slider-img w-100" />
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+      <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={StockImg1}  className='d-block w-100 h-400'/>
+    </div>
+    <div class="carousel-item">
+      <img src={StockImg2} className='d-block w-100 h-400'/>
+    </div>
+    <div class="carousel-item">
+      <img src={StockImg3} className='d-block w-100 h-400 '/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div>
 
         <h1 className='event-heading '>Welcome to Swamiji Education & Investment Hub...!<br />.......</h1>
         <div class="row">
