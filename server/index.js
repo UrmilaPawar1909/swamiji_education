f813866cf22e64a67e8aad4c72265fe1dde043e8
UrmilaPawar1909 <urmilapawar221@gmail.com>
2024-02-01 23:52:35 +0530

    server added

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import  express from "express";
+
+const app = express();
+
+app.get('/ping',(req, res)=>{
+
+    res.json("pong")
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`server is running on Port ${PORT}`)
+});
