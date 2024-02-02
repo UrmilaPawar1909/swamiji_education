1d30dfeb85535b544cb989721166f47df922117f
gita321 <gitanjalikokate921@gmail.com>
2024-02-02 09:52:37 -1200

    adding contact page

--- src/views/Contactus/Contactus.js
+++ src/views/Contactus/Contactus.js
@@ -10,39 +10,42 @@ import ContactImg from './img/contact.png';
 function Contactus() {
   return (<>
     <Navbar />
-    <div className='contact-container'>
-      <h2 className='main-heading '>Contact Details☎</h2>
+
+      <h2 className='main-heading mt-4 '> <b> Contact Details☎ </b></h2>
       <hr/>
+      <div className='contact-container'>
       <div class="row">
-        <div class="col-md-12 d-md-flex mt-5 justify-content-center ">
+        <div class="col-md-12 d-md-flex mt-2 justify-content-center ">
           <div class="col-md-5 mt-md-5 ms-4">
             <h1 class="fw-bolder fs-1 ">Get In Touch </h1>
-            <p> Have in query? or want to know more? <br />Contact us and we will be happy to help.</p>
-            <div className='location-container'>
+            <p className='mt-4 text-secondary'> <b>Have in query? or want to know more? <br />Contact us and we will be happy to help...!</b></p>
+            <hr/>
+            <hr/>
+            <div className='location-container mt-4'>
               <div className='location-logo'>
                 <img src={LocationImg} width={70} className='image'/>
               </div>
               <div className='location-description'>
-                <p className='address-title fs-2 text-danger'>Our Address</p>
-                <p>Unit 421, 4th floor, Goodwill Square Mall, Dhanori, Pune</p>
+                <p className='address-title fs-2 text-danger mt-2'>  ● <b>Our Address</b></p>
+                <p className='fs-5 text-primary'> Unit 421, 4th floor, Goodwill Square Mall, Dhanori,Pune</p>
               </div>
             </div>
-            <div className='email-container '>
+            <div className='email-container mt-4 '>
               <div className='email-logo'>
                 <img src={GmailImg} width={70} className='image'/>
               </div>
               <div className='email-description'>
-                <p className='email-title fs-2 text-danger'>E-mail</p>
-                <a> skeducationandinvestmenthub82@gmail.com </a>
+                <p className='email-title fs-2 text-danger mt-2'>  ● <b> E-mail</b></p>
+                <a className='fs-5 design-text'> skeducationandinvestmenthub82@gmail.com </a>
               </div>
             </div>
-            <div className='contact-container '>
+            <div className='contact-container mt-4'>
               <div className='contact-logo'>
                 <img src={ContactImg} width={70} className='image'/>
               </div>
               <div className='contact-description'>
-                <p className='contact-title fs-2 text-danger'>Contact </p>
-                <a> 9226985252 </a>
+                <p className='contact-title fs-2 text-danger mt-2'>  ● <b>Contact</b>  </p>
+                <a className='fs-5 design-text'> +91-9226985252 </a>
               </div>
             </div>
           </div>
