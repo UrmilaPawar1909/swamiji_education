df9e4f8334e09b3b1f23db556472f8697c4711ab
gita321 <gitanjalikokate921@gmail.com>
2024-04-06 14:35:13 +0530

    adding in open demat account

--- client/src/views/Openademataccount/Openademataccount.js
+++ client/src/views/Openademataccount/Openademataccount.js
@@ -14,10 +14,14 @@ function Openademataccount() {
             <div class="col-md-12 d-md-flex mt-5 justify-content-center ">
               
                 <div class="col-md-5 ">
+                <p class="text-uppercase account-text fs-3 fw-bold"> what is a demat account</p>
+              <br/>
+              <p fs-5 account-second-text  >A Demat (short for Dematerialized) is an electronic account used to hold and manage investments like Stocks, Bonds, Mutual Funds in a digital format. Just as you need to have a bank account to deposit your money and earn interest, you need to open a Demat account to buy or sell financial securities such as Stocks, Mutual Funds, Equity Traded Funds (ETFs), SGBs and more. When you buy stocks or shares, they will get credited to your account.
 
-                    <img src={DemetImg1}
-                        class="ms-md-5 d-block justify-content-center h-md-100 w-75 offset-1"/>
-                        
+When you sell the stocks or shares you have purchased, they will get debited from your account.<br/>
+
+By opening a Demat account, you do not need to transfer your share certificates physically to the buyer. You need to link your bank account with your Demat account, through which you can easily buy and sell shares. It does not only save your time, but it also reduces the transaction cost.</p>
+    
                 </div>
                 <From/>
 
